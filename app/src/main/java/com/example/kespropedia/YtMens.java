5dac19991915a5774be4890f53f63dd32c441f00
fdelmhmmad <fmw20311@outlook.com>
2020-10-30 21:06:07 +0800

    done play yt vid mens

---
+++ app/src/main/java/com/example/kespropedia/YtMens.java
@@ -0,0 +1,32 @@
+package com.example.kespropedia;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class YtMens extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.yt_mens, container, false);
+    }
+
+//    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//
+//        view.findViewById(R.id.button_third).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                NavHostFragment.findNavController(ThirdFragment.this)
+//                        .navigate(R.id.action_ThirdFragment_to_homeFragment);
+//            }
+//        });
+//    }
+}
